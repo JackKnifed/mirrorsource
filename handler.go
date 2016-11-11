7517564871a968fadb1200506228ee7977c03f57
Jack Hayhurst <jhayhurst@liquidweb.com>
2016-11-10 21:45:40 -0500

    dinked around with some stuff in the middle of being slammed - committing to get them in

--- handler.go
+++ handler.go
@@ -1,39 +1,61 @@
 package main
 
-type sourceConfig struct {
-	name           string            "json:name"
-	upstreamDriver string            "json:type"
-	upstream       upstreamInterface "json:upstream"
-	version        versionType       "json:version"
-	output         []outputType      "json:specFile"
-}
+import (
+	"net/http"
+	textTempl "text/template"
+)
 
-type upstreamTarget interface {
-	func check()bool
-	func download() error
-	func build() error
-}
+// type sourceConfig struct {
+// 	name           string            "json:name"
+// 	upstreamDriver string            "json:type"
+// 	upstream       upstreamTarget "json:upstream"
+// 	output         []outputType      "json:specFile"
+// }
+
+// type upstreamTarget interface {
+// 	check() error
+// 	version() string
+// 	download() error
+// }
 
 type versionType struct {
-	fmt  string   "json:fmt"
-	cur  string   "json:cur"
-	past []string "json:past"
+	fmt  string   `json:"fmt"`
+	cur  string   `json:"cur"`
+	past []string `json:"past"`
 }
 
+func (v versionType) nextPossible() (possible []string ) {
+	fmtChunks := strings.Split(v.fmt, "%v")
+	remainingChunks := fmtChunks
+
+	var possibleSubs []string
+	for len(fmtChunks) > 0 {
+		prefix := strings.TrimSuffix(v.cur, fmtChunks[len(fmtChunks)-1])
+
+	}
+}
+
+
 type outputType struct {
-	active      bool   "json:active"
-	gitRepo     string "json:gitRepo"
-	location    string "json:location"
-	outputID    int    "json:outputID"
-	outputPrune int    "json:outputPrune"
+	active      bool   `json:"active"`
+	gitRepo     string `json:"gitRepo"`
+	location    string `json:"location"`
+	outputID    int    `json:"outputID"`
+	outputPrune int    `json:"outputPrune"`
 }
 
 type httpResponseTracker struct {
-	driver string "json:driver"
-	url    string "json:url"
+	version versionType `json:"version"`
+	driver string `json:"driver"`
+	url    string `json:"url"`
 }
 
-func ServeSource() {
-	var configs []sourceConfig
+// func (upstream httpResponseTracker) check() error {
 
-}
+
+// 	req, _ := http.NewRequest("GET", upstream.Replace(upstream.url), nil)
+	
+
+
+
+// }
