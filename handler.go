c96335d9f9715f3d1e818dd95a607751a2743782
James Hayhurst <James.A.Hayhurst@gmail.com>
2016-11-11 23:57:48 -0500

    added a few helper functions that imma need
    they should help me to calculate the next possible version
    
    based on a current version string, and the bits in between

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	textTempl "text/template"
+	"strconv"
 )
 
 // type sourceConfig struct {
@@ -24,13 +25,52 @@ type versionType struct {
 	past []string `json:"past"`
 }
 
+func stringSubtraction(s string, chunks []string) ([]string, error) {
+	var result []string
+	remaining := strings.TrimLeft(chunks[0], s)
+
+	for i := 1; i < len(chunks) ; i++{
+		if !strings.Contains(remaining, chunks[i]) {
+			return []string{}, errors.New(`chunk [%q] not found in [%q]
+			original string [%q]
+			cut chunks %#v
+			parts gotten %#v`,
+			chunks[i], remaining, s, chunks, result)
+		}
+		parts := strings.SplitN(remaining, chunks[i], 2)
+		result = append(result, parts[0])
+		remaining = parts[1]
+	}
+
+	if remaining != "" {
+		result = append(result, remaining)
+	}
+
+	return result, nil
+}
+
+func stringsToInts(s []string) ([]int, error) {
+	var results []int
+	for _, s := range strings {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return []int{}, errors.New("failed - string [%q] is not an int - %v", s, err)
+		}
+		results = append(results, i)
+	}
+	return results
+}
+
 func (v versionType) nextPossible() (possible []string ) {
 	fmtChunks := strings.Split(v.fmt, "%v")
-	remainingChunks := fmtChunks
+	unprocessedPrefix := v.cur
+	var processedSuffix []string
 
-	var possibleSubs []string
 	for len(fmtChunks) > 0 {
+		suffix := fmtChunks[len(fmtChunks)-1]
 		prefix := strings.TrimSuffix(v.cur, fmtChunks[len(fmtChunks)-1])
+		parts := string.Split(prefix, fmtChunks[len(fmtChunks)-2])
+		versionNumber := parts[len(parts)-1]
 
 	}
 }
