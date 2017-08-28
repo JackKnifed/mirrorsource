1be19473787df91913d678e486c2ec142287e3b3
Jack Hayhurst <jakdept@gmail.com>
2017-08-28 15:53:47 -0400

    updated names

--- md5verify.go
+++ md5verify.go
@@ -12,24 +12,24 @@ import (
 )
 
 type MD5Verify struct {
-	errCh      chan<- error // inherited
-	fileLoc    string       // inherited
+	ErrCh      chan<- error // inherited
+	FileLoc    string       // inherited
 	BaseFmt    string       // inherited
 	HashURLFmt string
 }
 
 func (v *MD5Verify) Verify(basename string) bool {
-	filename := filepath.Join(v.fileLoc, basename)
+	filename := filepath.Join(v.FileLoc, basename)
 	f, err := os.Open(filename)
 	if err != nil {
-		v.errCh <- fmt.Errorf("failed to open file for checksumming - %v", err)
+		v.ErrCh <- fmt.Errorf("failed to open file for checksumming - %v", err)
 		return false
 	}
 	defer f.Close()
 
 	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
-		v.errCh <- fmt.Errorf("problem md5'ing file - %s - %v", filename, err)
+		v.ErrCh <- fmt.Errorf("problem md5'ing file - %s - %v", filename, err)
 		return false
 	}
 	actualSum := fmt.Sprintf("%x", h.Sum(nil))
@@ -37,20 +37,20 @@ func (v *MD5Verify) Verify(basename string) bool {
 	var version []interface{}
 	_, err = fmt.Sscanf(basename, v.BaseFmt, version...)
 	if err != nil {
-		v.errCh <- fmt.Errorf("problem decoding version information - %v", err)
+		v.ErrCh <- fmt.Errorf("problem decoding version information - %v", err)
 		return false
 	}
 
 	resp, err := http.Get(fmt.Sprintf(v.HashURLFmt, version...))
 	if err != nil {
-		v.errCh <- fmt.Errorf("problem retrieving remote hash - %v", err)
+		v.ErrCh <- fmt.Errorf("problem retrieving remote hash - %v", err)
 		return false
 	}
 	defer resp.Body.Close()
 
 	correctBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		v.errCh <- fmt.Errorf("problem retrieving remote hash - %v", err)
+		v.ErrCh <- fmt.Errorf("problem retrieving remote hash - %v", err)
 		return false
 	}
 	correctSum := strings.Fields(string(correctBytes))[0]
