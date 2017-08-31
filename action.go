cb2e8874b97a96db894eec7051dddcfba21bf0a5
Jack Hayhurst <jakdept@gmail.com>
2017-08-31 18:34:14 -0400

    moved over md5 stuff 👍

--- action.go
+++ action.go
@@ -1,6 +1,7 @@
 package mirrorsource
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
 	"fmt"
 	"io"
@@ -30,6 +31,44 @@ func (a *Sha1Verify) Do(v *Version) error {
 	defer f.Close()
 
 	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return fmt.Errorf("problem sha1'ing file - %s - %v", filename, err)
+	}
+	actualSum := fmt.Sprintf("%x", h.Sum(nil))
+
+	resp, err := http.Get(v.Format(a.HashURLFmt))
+	if err != nil {
+		return fmt.Errorf("problem retrieving remote hash - %v", err)
+	}
+	defer resp.Body.Close()
+
+	correctBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("problem retrieving remote hash - %v", err)
+	}
+	correctSum := strings.Fields(string(correctBytes))[0]
+
+	if correctSum != actualSum {
+		return fmt.Errorf("%s hash does not match", v.String())
+	}
+	return nil
+}
+
+type Md5Verify struct {
+	FileLoc    string // inherited
+	FileFmt    string
+	HashURLFmt string
+}
+
+func (a *Md5Verify) Do(v *Version) error {
+	filename := filepath.Join(a.FileLoc, v.Format(a.FileFmt))
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("failed to open [%s] for checksumming - %v", filename, err)
+	}
+	defer f.Close()
+
+	h := md5.New()
 	if _, err := io.Copy(h, f); err != nil {
 		return fmt.Errorf("problem sha1'ing file - %s - %v", filename, err)
 	}
